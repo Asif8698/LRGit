6501c3ef061f0f834646a87692c9782c74c30811
Asif8698 <parvezasif9960@gmail.com>
2020-11-23 13:46:05 +0530

    This is my Second Commit

--- Action.c
+++ Action.c
@@ -1,8 +1,6 @@
 Action()
 {
-	return 0;
-}
-
+	//This is my first GIT Pull and Commit
 	web_custom_request("web_custom_request",
 		"URL=www.example.com",
 		"Method=GET",
@@ -11,3 +9,8 @@ Action()
 		"Referer=",
 		"Body=",
 		LAST);
+
+	return 0;
+}
+
+	
